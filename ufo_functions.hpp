ff30bf2a21fdd1cec3a11cb31b12395961e40752
Brian Gitau <82699217+BrianGK314@users.noreply.github.com>
2021-05-13 15:57:06 +0300

    Create ufo_functions.hpp

---
+++ ufo_functions.hpp
@@ -0,0 +1,10 @@
+#include <vector>
+
+// Declare functions
+void display_misses(int misses);
+void greeting();
+void end_game(std::string codeword,std::string answer);
+void display_status(std::vector<char> incorrect,std::string answer);
+
+
+
